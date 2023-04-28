4b3a39e67907d6bcb263cd83577a641a45ee0b35
sid-leha <132030456+sid-leha@users.noreply.github.com>
2023-04-28 15:02:37 +0700

    Update test.java

--- src/test.java
+++ src/test.java
@@ -6,7 +6,7 @@ public class test {
         System.out.printf("Hello SID !");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 17; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.

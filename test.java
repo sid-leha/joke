4616207a7874a71acaf7f78b6332c6f8a862426d
sid <sidorenkovleha@ya.ru>
2023-04-28 14:51:18 +0700

    6 commit

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test {
+
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello SID 22222333333dgdfsgdsfgdsfg!!!33hghfghfghdfghfdghfghdfhgf33an333d!!! dfgdgdwelcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+
+}
